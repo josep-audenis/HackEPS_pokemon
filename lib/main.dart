a9f88b66f02b737c91d50187a46fb25d30b96c46
DSusagna <david.susagna.h@gmail.com>
2024-11-23 21:27:20 +0100

    main ok

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const PokedexScreen(), // Aquí se referencia la pantalla de login
+      home: const LoginScreen(), // Aquí se referencia la pantalla de login
     );
   }
 }
