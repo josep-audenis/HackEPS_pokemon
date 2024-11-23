cdcaeb8d984f9baf7437bf8f3fca54dc015fa247
Josep Audenis <pepaudenisr@gmail.com>
2024-11-23 19:10:00 +0100

    little modification

--- lib/providers/event_provider.dart
+++ lib/providers/event_provider.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import '../services/api_service.dart';
+import 'dart:async';
 
 class EventProvider extends ChangeNotifier {
   final ApiService _apiService = ApiService();
 
+  Timer? _timer;
   Set<String> _locationCodes = {};
   Set<String> get locationCodes => _locationCodes;
 
@@ -11,44 +13,76 @@ class EventProvider extends ChangeNotifier {
   Future<void> loadLocationCodes() async {
     try {
       final response = await _apiService.request(endpoint: 'pokemons', method: 'GET');
-      
-      List<dynamic> pokemons = response;
 
+      if (response is List<dynamic>) {
+        List<dynamic> pokemons = response;
 
-    if (response is List<dynamic>) {
-      List<dynamic> pokemons = response;
+        for (var pokemon in pokemons) {
+          if (pokemon is Map<String, dynamic> && pokemon['location_area_encounters'] != null) {
+            List<dynamic> locations = pokemon['location_area_encounters'];
 
-      // Parsear todas las location_area_encounters
-      for (var pokemon in pokemons) {
-        // Verificar si 'location_area_encounters' es una lista
-        if (pokemon is Map<String, dynamic> && pokemon['location_area_encounters'] != null) {
-          List<dynamic> locations = pokemon['location_area_encounters'];
-
-          // Iterar sobre cada localización y agregar al set
-          for (var location in locations) {
-            if (location is String) {
-              _locationCodes.add(location);
+            for (var location in locations) {
+              if (location is String) {
+                _locationCodes.add(location);
+              }
             }
           }
         }
+
+        notifyListeners();
+      } else {
+        print('ERROR: La respuesta no es una lista, es de tipo ${response.runtimeType}');
       }
-
-      notifyListeners();
-    } else {
-      print('ERROR: La respuesta no es una lista, es de tipo ${response.runtimeType}');
-    }
-      //parseja totes les location_area_encounter
-      // for (var pokemon in pokemons) {
-      //   //print('${pokemon.toString()}\n');
-      //   if (pokemon is Map<String, dynamic> && pokemon['location_area_encounters'] != null) {
-      //     _locationCodes.add(pokemon['location_area_encounters']);
-      //   }
-
-      // }
-
-      // notifyListeners();
     } catch (e) {
       print('ERROR: couldn\'t parase pokemons: $e');
     }
   }
+
+  // void startMonitoringZones(int intervalInSeconds) {
+  //   _timer?.cancel();
+
+  //   _timer = Timer.periodic(Duration(seconds: intervalInSeconds), (timer) async {
+  //     await _checkZones();
+  //   });
+  // }
+
+  // void stopMonitoringZones() {
+  //   _timer?.cancel();
+  // }
+
+  // Future<void> _checkZones(int team_id) async {
+  //   try {
+  //     final response = await _apiService.request(endpoint: 'zones', method: 'GET');
+  //     List<dynamic> zones = response;
+
+  //     for (var zone in zones) {
+  //       if (zone['cooldown'] == 0) {
+  //         final zoneId = zone['zone_id'];
+  //         final teamId = team_id; 
+
+  //         await _apiService.request(
+  //           endpoint: 'event',
+  //           method: 'POST',
+  //           headers: {'Content-Type': 'application/json'},
+  //           body: {'zone_id': zoneId, 'team_id': teamId},
+  //         );
+
+  //         print('Pokémon capturado en la zona: $zoneId');
+  //       } else {
+  //         print('Zona ${zone['zone_id']} está en cooldown por ${zone['cooldown']} segundos.');
+  //       }
+  //     }
+  //   } catch (e) {
+  //     print('ERROR: Fallo al consultar las zonas: $e');
+  //   }
+  // }
+
+  // @override
+  // void dispose() {
+  //   _timer?.cancel();
+  //   super.dispose();
+  // }
+
+
+
 }
