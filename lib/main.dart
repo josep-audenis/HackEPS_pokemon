c1400c414cf22c43b26ff4b599566baa498de16e
Josep Audenis <pepaudenisr@gmail.com>
2024-11-24 08:51:21 +0100

    debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+		debugShowCheckedModeBanner: false,
       title: 'LSPokedex',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
